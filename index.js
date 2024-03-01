2f151168a3d410486a4d237d4d4d2e361779999f
Jared Kuntz <timeo.kalar.ultros@gmail.com>
2024-03-01 13:30:09 -0600

    Change from reply to a delete and repost

--- index.js
+++ index.js
@@ -13,25 +13,28 @@ client.on("messageCreate", function(message) {
   const content = message.content;
 
   if (content.includes(process.env.TIKTOK_PREFIX)) {
-    message.reply(
-        `This looks like a tiktok. Here's the fixed embed!  
-${content.replace('tiktok', 'vxtiktok')}`);
+    message.channel.send(
+        `Hey ${message.author.toString() }, this looks like a tiktok. I fixed the embed for ya!  
+${content.replace('tiktok', 'vxtiktok')}` );
 console.log(`Successfully fixed tiktok ${content}`)
+message.delete()
     
   }
 
   if (content.includes(process.env.TWITTER_PREFIX_SHORT) || content.includes(process.env.TWITTER_PREFIX_FULL)) {
-    message.reply(
-        `This looks like tweet. Here's the fixed embed!  
+    message.channel.send(
+      `Hey ${message.author.toString() }, this looks like a tweet. I fixed the embed for ya!  
 ${content.replace('twitter', 'fxtwitter')}`);
     console.log(`Successfully fixed tweet ${content}`)
+    message.delete()
   }
 
   if (content.includes(process.env.INSTA_PREFIX)) {
-    message.reply(
-        `This looks like it's from Instagram. Here's the fixed embed!  
+    message.channel.send(
+      `Hey ${message.author.toString() }, this looks like it's from Insta. I fixed the embed for ya!  
 ${content.replace('instagram', 'ddinstagram')}`);
     console.log(`Successfully fixed insta ${content}`)
+    message.delete()
   }
 });
 client.login(process.env.DISCORD_TOKEN);
